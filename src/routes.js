6ba02c50ee0ef788db4e1101624da413488e06f8
Min <owner@ownerdeMacBook-Pro.local>
2022-10-25 10:54:29 +1100

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,56 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/dashboard';
+import SimpleLayout from './layouts/simple';
+//
+import BlogPage from './pages/BlogPage';
+import UserPage from './pages/UserPage';
+import Assignment from './pages/Assignment';
+import LoginPage from './pages/LoginPage';
+import Page404 from './pages/Page404';
+import ProductsPage from './pages/ProductsPage';
+import DashboardAppPage from './pages/DashboardAppPage';
+import AssignmentDetail from './pages/AssignmentDetail';
+import AssignmentMark from './pages/AssignmentMark';
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  const routes = useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" />, index: true },
+        { path: 'app', element: <DashboardAppPage /> },
+        { path: 'user', element: <UserPage /> },
+        { 
+          path:'assignment', 
+          element: <Assignment /> ,
+        },
+        { path: 'assignmentDetail', element: <AssignmentDetail/>},
+        { path: 'assignmentMark', element: <AssignmentMark/>},
+        { path: 'products', element: <ProductsPage /> },
+        { path: 'blog', element: <BlogPage /> },
+      ],
+    },
+    {
+      path: 'login',
+      element: <LoginPage />,
+    },
+    {
+      element: <SimpleLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" />, index: true },
+        { path: '404', element: <Page404 /> },
+        { path: '*', element: <Navigate to="/404" /> },
+      ],
+    },
+    {
+      path: '*',
+      element: <Navigate to="/404" replace />,
+    },
+  ]);
+
+  return routes;
+}
